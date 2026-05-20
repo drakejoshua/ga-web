0e65757eb933c4f084a63cf0f1690c83e15bcbc2
Joshua Mabawonku <drakejoshua086@gmail.com>
2026-05-20 16:53:54 +0100

    feat: add anchor links to pages for GA tracking

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function About() {
     return (
         <div className='page'>
             <span>
                 This is the about page
             </span>
+
+            <div>
+                <Link to="/about">Go to about page</Link>
+                <Link to="/contact">Go to contact page</Link>
+            </div>
         </div>
     )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Contact() {
     return (
         <div className='page'>
             <span>
                 This is the contact page
             </span>
+
+            <div>
+                <Link to="/about">Go to about page</Link>
+                <Link to="/contact">Go to contact page</Link>
+            </div>
         </div>
     )
 }
